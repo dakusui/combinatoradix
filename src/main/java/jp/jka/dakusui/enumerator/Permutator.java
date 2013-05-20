3d1542c09f714af4f95e6abebb9dc0477866d0a9
Hiroshi Ukai <hiroshi.ukai@mail.rakuten.com>
2013-05-20 17:30:27 +0900

    Refactored fields and methods. (pulled up to Enumerator class).

--- src/main/java/jp/jka/dakusui/enumerator/Permutator.java
+++ src/main/java/jp/jka/dakusui/enumerator/Permutator.java
@@ -5,28 +5,6 @@ import java.util.List;
 
 
 public class Permutator<T> extends Enumerator<T> {
-	int k;
-	
-	public Permutator(List<T> list, int k) {
-		super(list, nPk(list.size(), k));
-		this.k = k;
-	}
-	
-	protected Permutator(List<T> list, long maxIndex) {
-		super(list, maxIndex);
-	}
-	
-	@Override
-	protected List<T> get_Protected(long index) {
-		int[] seq = index2locator(index, this.list.size() - k + 1, k);
-		List<T> tmp = new LinkedList<T>(this.list);
-		List<T> ret = new LinkedList<T>();
-		for (int i : seq) {
-			ret.add(tmp.remove((int)i));
-		}
-		return ret;
-	}
-
 	static int[] index2locator(long index, int lsradix, int k) {
 		int[] seq = new int[k];
 		long c = index;
@@ -37,5 +15,25 @@ public class Permutator<T> extends Enumerator<T> {
 			radix++;
 		}
 		return seq;
+	}
+	
+	public Permutator(List<T> items, int k) {
+		super(items, k);
+	}
+
+	@Override
+	protected List<T> get_Protected(long index) {
+		int[] seq = index2locator(index, this.items.size() - k + 1, k);
+		List<T> tmp = new LinkedList<T>(this.items);
+		List<T> ret = new LinkedList<T>();
+		for (int i : seq) {
+			ret.add(tmp.remove((int)i));
+		}
+		return ret;
+	}
+
+	@Override
+	protected long size() {
+		return nPk(this.items.size(), this.k);
 	}
 }
