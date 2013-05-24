a9894bccbc1a48bd92eba3779b746af74ec3402e
hiroshi <dakusui@gmail.com>
2013-05-25 04:53:07 +0900

    Changed package name to com.github... in order to make it match with groupId.

---
+++ src/main/java/com/github/dakusui/enumerator/Enumerator.java
@@ -0,0 +1,84 @@
+package com.github.dakusui.enumerator;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public abstract class Enumerator<T> implements Iterator<List<T>>{
+
+	static long factorial(long n) {
+		if (n < 0) {
+			String msg = "n cannot be less than 0. (" + n + ")";
+			throw new RuntimeException(msg);
+		}
+		if (n == 0) return 1;
+		long ret = 1;
+		for (long i = n ; i > 1; i--) {
+			ret *= i;
+		}
+		return ret;
+	}
+	
+	static long nCk(long n, long k) {
+		return nPk(n, k) / factorial(k);
+	}
+	
+	static long nHk(int n, int k) {
+		long ret = nCk(n+k-1, k);
+		return ret;
+	}
+
+	static long nPk(long n, long k) {
+		long ret = 1;
+		for (long i = n; i > n-k; i-- ) {
+			ret *= i;
+		}
+		return ret;
+	}
+	
+	private long enumSize;
+
+	private long index;
+	
+	protected int k;
+
+	protected List<T> items;
+
+	protected Enumerator(List<T> items, int k) {
+		this.items = items;
+		this.k = k;
+		this.enumSize = size();
+	}
+	
+	public List<T> get(long index) {
+		if (index < enumSize) {
+			return get_Protected(index);
+		}
+		String msg = String.format("Index (%d) must be less than %d", index, this.enumSize);
+		throw new IndexOutOfBoundsException(msg);
+	}
+	
+	protected abstract List<T> get_Protected(long index);
+
+	public abstract long size();
+	
+	@Override
+	public boolean hasNext() {
+		return this.index < this.enumSize;
+	}
+
+	@Override
+	public List<T> next() {
+		if (!hasNext()) {
+			String message = "No more element in this enumberator.";
+			throw new NoSuchElementException(message);
+		}
+		return get_Protected(this.index++);
+	}
+
+	@Override
+	public void remove() {
+		String message = "This operation is not supported.";
+		throw new UnsupportedOperationException(message);
+	}
+}
---
+++ src/test/java/com/github/dakusui/enumerator/PermutatorTest.java
@@ -0,0 +1,98 @@
+package com.github.dakusui.enumerator;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.github.dakusui.enumerator.Enumerator;
+import com.github.dakusui.enumerator.Permutator;
+
+public class PermutatorTest extends EnumeratorTestBase {
+	@Test
+	public void test_nP0() {
+		Enumerator<String> p = new Permutator<String>(testset1(), 0);
+		assertEquals(true, p.hasNext());
+		assertEquals(toList(), p.next());
+		assertEquals(false, p.hasNext());
+	}
+
+	@Test
+	public void test_nP1() {
+		Enumerator<String> p = new Permutator<String>(testset1(), 1);
+		assertEquals(true, p.hasNext());
+		assertEquals(toList("A"), p.next());
+		assertEquals(true, p.hasNext());
+		assertEquals(toList("B"), p.next());
+		assertEquals(true, p.hasNext());
+		assertEquals(toList("C"), p.next());
+		assertEquals(true, p.hasNext());
+		assertEquals(toList("D"), p.next());
+		assertEquals(true, p.hasNext());
+		assertEquals(toList("E"), p.next());
+		assertEquals(false, p.hasNext());
+	}
+	
+	@Test
+	public void test_nP2() {
+		Enumerator<String> p = new Permutator<String>(testset1(), 2);
+		assertEquals(true, p.hasNext());
+		assertEquals(toList("A", "B"), p.next());
+		assertEquals(true, p.hasNext());
+		assertEquals(toList("A", "C"), p.next());
+		assertEquals(true, p.hasNext());
+		assertEquals(toList("A", "D"), p.next());
+		assertEquals(true, p.hasNext());
+		assertEquals(toList("A", "E"), p.next());
+		assertEquals(true, p.hasNext());
+		assertEquals(toList("B", "A"), p.next());
+		assertEquals(true, p.hasNext());
+		assertEquals(toList("B", "C"), p.next());
+		assertEquals(true, p.hasNext());
+		assertEquals(toList("B", "D"), p.next());
+		assertEquals(true, p.hasNext());
+		assertEquals(toList("B", "E"), p.next());
+		assertEquals(true, p.hasNext());
+		assertEquals(toList("C", "A"), p.next());
+		assertEquals(true, p.hasNext());
+		assertEquals(toList("C", "B"), p.next());
+		assertEquals(true, p.hasNext());
+		assertEquals(toList("C", "D"), p.next());
+		assertEquals(true, p.hasNext());
+		assertEquals(toList("C", "E"), p.next());
+		assertEquals(true, p.hasNext());
+		assertEquals(toList("D", "A"), p.next());
+		assertEquals(true, p.hasNext());
+		assertEquals(toList("D", "B"), p.next());
+		assertEquals(true, p.hasNext());
+		assertEquals(toList("D", "C"), p.next());
+		assertEquals(true, p.hasNext());
+		assertEquals(toList("D", "E"), p.next());
+		assertEquals(true, p.hasNext());
+		assertEquals(toList("E", "A"), p.next());
+		assertEquals(true, p.hasNext());
+		assertEquals(toList("E", "B"), p.next());
+		assertEquals(true, p.hasNext());
+		assertEquals(toList("E", "C"), p.next());
+		assertEquals(true, p.hasNext());
+		assertEquals(toList("E", "D"), p.next());
+		assertEquals(false, p.hasNext());
+	}
+	
+	@Test
+	public void test_nPn() {
+		Enumerator<String> p = new Permutator<String>(testset2(), 3);
+		assertEquals(true, p.hasNext());
+		assertEquals(toList("A", "B", "C"), p.next());
+		assertEquals(true, p.hasNext());
+		assertEquals(toList("A", "C", "B"), p.next());
+		assertEquals(true, p.hasNext());
+		assertEquals(toList("B", "A", "C"), p.next());
+		assertEquals(true, p.hasNext());
+		assertEquals(toList("B", "C", "A"), p.next());
+		assertEquals(true, p.hasNext());
+		assertEquals(toList("C", "A", "B"), p.next());
+		assertEquals(true, p.hasNext());
+		assertEquals(toList("C", "B", "A"), p.next());
+		assertEquals(false, p.hasNext());
+	}
+}
